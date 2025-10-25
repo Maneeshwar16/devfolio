66b43b4b6a655923a0e016499a1998e610427b85
Maneeshwar16 <122243997+Maneeshwar16@users.noreply.github.com>
2025-10-25 21:43:27 +0530

    Initial commit: Portfolio website with Next.js and PWA support

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+import withPWA from '@ducanh2912/next-pwa';
+
+const config = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/',
+  sw: 'service-worker.js',
+  skipWaiting: true,
+  runtimeCaching: [],
+})({
+  reactStrictMode: true,
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+});
+
+export default config;
